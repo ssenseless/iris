a5f1393db1deb5dd0a1ccba99935642d3c8b1b34
ssenseless <major.add.9@ou.edu>
2024-05-05 19:17:30 -0700

    post-simu.

--- simpleplot.py
+++ simpleplot.py
@@ -7,9 +7,6 @@ sns.set(style="white", color_codes=True)
 # fetch dataset
 iris = fetch_ucirepo(id=53)
 originaldf = iris.data.original
-temp = originaldf.iloc[0]
-originaldf.iloc[0] = originaldf.iloc[55]
-originaldf.iloc[55] = temp
 
 sns.pairplot(originaldf, hue='class', height=2, palette=sns.color_palette("tab10"))
 plt.show()
